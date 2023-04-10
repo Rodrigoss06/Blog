530087a0d88a9427f9fdb2bb251ef4cb8c8ccec7
Rodrigo <102317296+Rodrigoss06@users.noreply.github.com>
2023-04-10 17:32:51 +0000

    deploy

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -2,7 +2,7 @@
 
 function Copyright() {
     return(
-  <section className=" text-center ">
+  <section className=" text-end">
     
     <footer id="copyright" className="text-gray-500 text-sm w-full bottom-0 absolute">
       Copyright&copy; 2022 - Página creada por Rodrigo S.S. - Todos los derechos
